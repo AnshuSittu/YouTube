f07f947f07d0821113a35c75625f14196debbf0d
Anshu <anshu.possible@gmail.com>
2024-04-18 15:28:50 +0530

    toggle is working implimented

--- my-youtube/src/App.js
+++ my-youtube/src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Body from "./components/Body";
 import Head from "./components/Head";
+import store from "./utils/store";
+import { Provider } from "react-redux";
+
 
 function App() {
   return (
+    <Provider store={store}>
     <div>
       <Head />
       <Body/>
     </div>
+    </Provider>
   );
 }
 
--- my-youtube/src/components/Sidebar.js
+++ my-youtube/src/components/Sidebar.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+
 
 const Sidebar = () => {
-  return (
-    <div>
-      This is side bar
-    </div>
-  )
-}
+  const isMenuOPen = useSelector(store => store.app.isMenuOPen); // subscribing
 
-export default Sidebar
+  //early return pattern 
+  
+  if(!isMenuOPen) return null ;
+
+  return (
+    <div className="p-5 shadow-lg w-48">
+     <ul>
+        <li className="m-2">Home </li>
+        <li className="m-2">Shorts </li>
+        <li className="m-2">Videos</li>
+        <li className="m-2">Live</li>
+      </ul>
+
+      <h1 className="font-bold pt-5"> Subscriptions</h1>
+      <ul >
+        <li className="m-2">Music </li>
+        <li className="m-2">Sports </li>
+        <li className="m-2">Gaming </li>
+        <li className="m-2">Movies </li>
+      </ul>
+      <h1 className="font-bold pt-5"> WatchList</h1>
+      <ul>
+        <li className="my-2">History   </li>
+        <li className="my-2">Playlist   </li>
+        <li className="my-2">Watch Later </li>
+        <li className="my-2">Liked Videos  </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
